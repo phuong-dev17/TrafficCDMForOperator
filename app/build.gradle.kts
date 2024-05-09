a1136342f0564818d1593eb2c5a705e4bcc38749
phuong-dev17 <125834793+phuong-dev17@users.noreply.github.com>
2024-05-10 01:31:46 +0200

    enabler: update AGP and add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -43,13 +44,35 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.13.0")
+    implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.activity:activity-ktx:1.9.0")
 
+    // openid connect
     implementation("net.openid:appauth:0.11.1")
-    implementation("com.google.android.material:material:1.12.0-rc01")
-    implementation("androidx.navigation:navigation-fragment:2.7.7")
+
+    // Backport support for Java Datetime
+    implementation("org.threeten:threetenbp:1.6.0")
+
+    // Base64 decoder
+    implementation("commons-codec:commons-codec:1.15")
+
+    // Navigation
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+
+    // Moshi
+    implementation("com.squareup.moshi:moshi-kotlin:1.14.0")
+    ksp("com.squareup.moshi:moshi-kotlin-codegen:1.14.0")
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
+
+    //Logging Network Calls
+    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
+
+    // DateTimePicker
+    implementation("com.github.loperSeven:DateTimePicker:0.6.3")
 }
