c8d5e9cc560775f4f82249231cf18d7917a169a7
phuong-dev17 <125834793+phuong-dev17@users.noreply.github.com>
2024-05-22 00:21:14 +0200

    upgrade AGP version

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ buildscript {
 }
 
 plugins {
-    id("com.android.application") version "8.4.0" apply false
+    id("com.android.application") version "8.4.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
     id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
 }
