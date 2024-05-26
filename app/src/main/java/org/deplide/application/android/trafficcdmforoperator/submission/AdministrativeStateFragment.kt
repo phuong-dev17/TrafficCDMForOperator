ae2a8629139c63ce3ead6e732bdc0191d9e516c2
phuong-dev17 <125834793+phuong-dev17@users.noreply.github.com>
2024-05-26 22:02:30 +0200

    switch to use binding in administrative state

--- app/src/main/java/org/deplide/application/android/trafficcdmforoperator/submission/AdministrativeStateFragment.kt
+++ app/src/main/java/org/deplide/application/android/trafficcdmforoperator/submission/AdministrativeStateFragment.kt
@@ -6,14 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import org.deplide.application.android.trafficcdmforoperator.R
+import org.deplide.application.android.trafficcdmforoperator.databinding.FragmentAdministrativeStateBinding
 
 class AdministrativeStateFragment : Fragment(), StateFragmentDataUpdater {
+    private lateinit var binding: FragmentAdministrativeStateBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_administrative_state, container, false)
+        binding = FragmentAdministrativeStateBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun addStateFragmentDataUpdateListener(listener: StateFragmentDataUpdateListener) {
