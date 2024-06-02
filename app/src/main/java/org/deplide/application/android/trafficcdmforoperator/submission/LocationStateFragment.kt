bf3355ab4dd196d82eb9b3336e80b33a1646a676
phuong-dev17 <125834793+phuong-dev17@users.noreply.github.com>
2024-06-02 17:40:49 +0200

    Finish loading locations to UI

--- app/src/main/java/org/deplide/application/android/trafficcdmforoperator/submission/LocationStateFragment.kt
+++ app/src/main/java/org/deplide/application/android/trafficcdmforoperator/submission/LocationStateFragment.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.core.os.BundleCompat
 import androidx.core.widget.addTextChangedListener
 import com.squareup.moshi.JsonAdapter
@@ -89,7 +90,12 @@ class LocationStateFragment : BaseStateFragment() {
         val loadedLocations: Map<String, String> = parseLocationJsonString(locationJsonString)
 
         locations = makeLocationFullNameAsKeyInTheMap(loadedLocations)
-        Log.d(TAG, "loadLocations: $locations")
+
+        val adapter = ArrayAdapter<String>(
+            requireContext(),
+            R.layout.cell_location,
+            locations.keys.toList())
+        binding.edtLocationLocationState.setAdapter(adapter)
     }
 
     private fun makeLocationFullNameAsKeyInTheMap(loadedLocations: Map<String, String>) =
