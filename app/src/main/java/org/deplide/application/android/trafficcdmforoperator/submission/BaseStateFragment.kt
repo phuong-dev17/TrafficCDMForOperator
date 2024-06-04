cc2709d671c4df9d20603830072e239db17e36b2
phuong-dev17 <125834793+phuong-dev17@users.noreply.github.com>
2024-06-04 22:54:57 +0200

    refactor: move location json data reading to base state fragment

--- app/src/main/java/org/deplide/application/android/trafficcdmforoperator/submission/BaseStateFragment.kt
+++ app/src/main/java/org/deplide/application/android/trafficcdmforoperator/submission/BaseStateFragment.kt
@@ -1,10 +1,28 @@
 package org.deplide.application.android.trafficcdmforoperator.submission
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.Types
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.Reader
+import java.io.StringWriter
+import java.io.Writer
+import java.lang.reflect.Type
 
 open class BaseStateFragment: Fragment(), StateFragmentDataUpdater  {
     private val data: MutableMap<String, String> = mutableMapOf()
     private var dataUpdateListener: StateFragmentDataUpdateListener? = null
+    protected lateinit var locations: Map<String, String>
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        loadLocations()
+    }
 
     protected fun updateData(key: String, value: String) {
         data[key] = value
@@ -13,6 +31,51 @@ open class BaseStateFragment: Fragment(), StateFragmentDataUpdater  {
 
     private fun informListeners() {
         dataUpdateListener?.onStateFragmentDataUpdate(data)
+    }
+
+    private fun loadLocations() {
+        val locationJsonString = readLocationJsonFile()
+
+        val loadedLocations: Map<String, String> = parseLocationJsonString(locationJsonString)
+
+        locations = makeLocationFullNameAsKeyInTheMap(loadedLocations)
+    }
+
+    private fun makeLocationFullNameAsKeyInTheMap(loadedLocations: Map<String, String>) =
+        loadedLocations.map { (k, v) -> v to k }.toMap()
+    private fun parseLocationJsonString(locationJsonString: String): Map<String, String> {
+        val moshi: Moshi = Moshi.Builder().build()
+
+        val type: Type = Types.newParameterizedType(
+            MutableMap::class.java,
+            String::class.java,
+            String::class.java
+        )
+        val jsonAdapter: JsonAdapter<Map<String, String>> = moshi.adapter(type)
+
+        val loadedLocations: Map<String, String> = jsonAdapter.fromJson(locationJsonString)!!
+        return loadedLocations
+    }
+
+    /*
+     * This complete function is copied from accepted answer in Stackoverflow
+     * https://stackoverflow.com/questions/6349759/using-json-file-in-android-app-resources
+     */
+    private fun readLocationJsonFile(): String {
+        val inputStream = resources.openRawResource(org.deplide.application.android.trafficcdmforoperator.R.raw.places)
+        val writer: Writer = StringWriter()
+        val buffer = CharArray(1024)
+        try {
+            val reader: Reader = BufferedReader(InputStreamReader(inputStream, "UTF-8"))
+            var n: Int
+            while ((reader.read(buffer).also { n = it }) != -1) {
+                writer.write(buffer, 0, n)
+            }
+        } finally {
+            inputStream.close()
+        }
+
+        return writer.toString()
     }
 
     override fun addStateFragmentDataUpdateListener(listener: StateFragmentDataUpdateListener) {
--- app/src/main/java/org/deplide/application/android/trafficcdmforoperator/submission/LocationStateFragment.kt
+++ app/src/main/java/org/deplide/application/android/trafficcdmforoperator/submission/LocationStateFragment.kt
@@ -10,9 +10,6 @@ import android.widget.ArrayAdapter
 import android.widget.ListPopupWindow
 import androidx.core.os.BundleCompat
 import androidx.core.widget.addTextChangedListener
-import com.squareup.moshi.JsonAdapter
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.Types
 import org.deplide.application.android.trafficcdmforoperator.R
 import org.deplide.application.android.trafficcdmforoperator.databinding.FragmentLocationStateBinding
 import org.deplide.application.android.trafficcdmforoperator.submission.data.version_0_0_7.SubmissionData
@@ -20,12 +17,6 @@ import org.deplide.application.android.trafficcdmforoperator.submission.submitti
 import org.deplide.application.android.trafficcdmforoperator.submission.util.DateTimeHelper.Companion.convertUTCTimeToSystemDefault
 import org.deplide.application.android.trafficcdmforoperator.submission.util.DateTimeHelper.Companion.getCurrentDateTime
 import org.deplide.application.android.trafficcdmforoperator.submission.util.DateTimePicker
-import java.io.BufferedReader
-import java.io.InputStreamReader
-import java.io.Reader
-import java.io.StringWriter
-import java.io.Writer
-import java.lang.reflect.Type
 
 
 class LocationStateFragment : BaseStateFragment() {
@@ -33,7 +24,6 @@ class LocationStateFragment : BaseStateFragment() {
     private var initialData: SubmissionData? = null
     private var editMode: String? = null
     private lateinit var dateTimePicker: DateTimePicker
-    private lateinit var locations: Map<String, String>
     private val listPopupWindow by lazy { ListPopupWindow(requireContext()) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -61,7 +51,6 @@ class LocationStateFragment : BaseStateFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        loadLocations()
         setCurrentTimeForTimeField()
         setDefaultForFields()
         configureListeners()
@@ -84,52 +73,6 @@ class LocationStateFragment : BaseStateFragment() {
             updateData(SubmissionData.FIELD_LOCATION,
                 defaultLocationType)
         }
-    }
-
-    private fun loadLocations() {
-        val locationJsonString = readLocationJsonFile()
-
-        val loadedLocations: Map<String, String> = parseLocationJsonString(locationJsonString)
-
-        locations = makeLocationFullNameAsKeyInTheMap(loadedLocations)
-    }
-
-    private fun makeLocationFullNameAsKeyInTheMap(loadedLocations: Map<String, String>) =
-        loadedLocations.map { (k, v) -> v to k }.toMap()
-
-    private fun parseLocationJsonString(locationJsonString: String): Map<String, String> {
-        val moshi: Moshi = Moshi.Builder().build()
-
-        val type: Type = Types.newParameterizedType(
-            MutableMap::class.java,
-            String::class.java,
-            String::class.java
-        )
-        val jsonAdapter: JsonAdapter<Map<String, String>> = moshi.adapter(type)
-
-        val loadedLocations: Map<String, String> = jsonAdapter.fromJson(locationJsonString)!!
-        return loadedLocations
-    }
-
-    /*
-     * This complete function is copied from accepted answer in Stackoverflow
-     * https://stackoverflow.com/questions/6349759/using-json-file-in-android-app-resources
-     */
-    private fun readLocationJsonFile(): String {
-        val inputStream = resources.openRawResource(org.deplide.application.android.trafficcdmforoperator.R.raw.places)
-        val writer: Writer = StringWriter()
-        val buffer = CharArray(1024)
-        try {
-            val reader: Reader = BufferedReader(InputStreamReader(inputStream, "UTF-8"))
-            var n: Int
-            while ((reader.read(buffer).also { n = it }) != -1) {
-                writer.write(buffer, 0, n)
-            }
-        } finally {
-            inputStream.close()
-        }
-
-        return writer.toString()
     }
 
     private fun configureAccordingToEditMode() {
